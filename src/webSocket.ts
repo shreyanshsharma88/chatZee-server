12dd27e2d199f3282f2ecdb50aff1a6b54cbf596
shreyanshSharma88 <shreyanshsharma480@gmail.com>
2024-11-21 13:55:50 +0530

    fix

--- src/webSocket.ts
+++ src/webSocket.ts
@@ -13,49 +13,55 @@ webSocketServer.on(
     socket: WebSocket.WebSocket,
     metadata: { token: string; userId: string; username: string }
   ) => {
-    socket.on("message", async (res: string) => {
-      // TODO: THIS WONT WORK **MOST PROBABLY**
-      // TODO: OPTIMIZE THIS
+    try {
+      socket.on("message", async (res: string) => {
+        // TODO: THIS WONT WORK **MOST PROBABLY**
+        // TODO: OPTIMIZE THIS
 
-      const data = JSON.parse(res);
+        const data = JSON.parse(res);
 
-      const groupId = data.groupId;
-      const userId = data.userId;
+        const groupId = data.groupId;
+        const userId = data.userId;
 
-      const usersInGroup = await prisma.group_users.findMany({
-        where: {
-          group_id: groupId,
-        },
-        select: {
-          user_id: true,
-        },
-      });
+        const usersInGroup = await prisma.group_users.findMany({
+          where: {
+            group_id: groupId,
+          },
+          select: {
+            user_id: true,
+          },
+        });
 
-      const userIdsInGroup = usersInGroup.map(({ user_id }) => user_id);
+        const userIdsInGroup = usersInGroup.map(({ user_id }) => user_id);
 
-      const response = JSON.stringify({
-        sentBy: userId,
-        sentTo: groupId,
-        message: data.message.toString(),
-        id: randomUUID(),
-        time: data.time,
-        username: data.username,
-      });
+        const response = JSON.stringify({
+          sentBy: userId,
+          sentTo: groupId,
+          message: data.message?.toString(),
+          id: randomUUID(),
+          time: data.time,
+          username: data.username,
+        });
 
-      for (const [_, { socket, userId }] of TokenSocketMap.entries()) {
-        if (userIdsInGroup.includes(userId)) {
-          socket?.send(response);
+        for (const [_, { socket, userId }] of TokenSocketMap.entries()) {
+          if (userIdsInGroup.includes(userId)) {
+            socket?.send(response);
+          }
         }
-      }
 
-     await prisma.chats.create({
-        data: {
-          message: data.message,
-          sent_by: userId,
-          sent_to: groupId,
-        },
+        if (!!data.message) {
+          await prisma.chats.create({
+            data: {
+              message: data.message,
+              sent_by: userId,
+              sent_to: groupId,
+            },
+          });
+        }
       });
-    });
+    } catch (error) {
+      console.log(error);
+    }
   }
 );
 
